58271d3f3e0b1e819f22906b5ec37fbb13e93432
villysiu <villysiu@gmail.com>
2021-11-14 23:35:55 -0800

    commit tree 8ddfb26d5eb15e62e48e07b3b2b0e38e4d6d944f

--- index.js
+++ index.js
@@ -9,14 +9,22 @@ let map = (sourceArray, f) => {
 //[1,2,3,4,5]
 //sourceArray = [1, 2, true, "razmatazz"]
 let reduce = (sourceArray, f, startingPoint=null) => {
-    let result, i=0
-    if (startingPoint) {
-        result = f(startingPoint, sourceArray[i])
-        i++
-    } else {
-        result = f(sourceArray[i], sourceArray[i+1])
-        i+=2
-    }
+   let result 
+   let i
+    // if (startingPoint) {
+    //     result = f(startingPoint, sourceArray[i])
+    //     i++
+    // } else {
+    //     result = f(sourceArray[i], sourceArray[i+1])
+    //     i+=2
+    // }
+    [result, i] = startingPoint? (
+         [f(startingPoint, sourceArray[0]), 1]
+    ) : (
+         [f(sourceArray[0], sourceArray[1]), 2]
+
+    )
+    
 
     for (i; i<sourceArray.length; i++) {
         result = f(result, sourceArray[i])
